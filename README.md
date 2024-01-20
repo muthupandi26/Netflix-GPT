6290e7b3213be807df0a83691bb65ec4b03b072b
Muthupandi <p.muthupandi@tftus.com>
2024-01-20 22:14:06 +0530

    sign-in & sign-up validation done

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 - Create React App
 - Configured Tailwind CSS
 - Header
+- Routing of App
 - Login form
 - Sign In form
+- Form Validation
+- useRef Hook
 
 # Features
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+  };
+
+  const handleButtonClick = () => {
+    // checkValidData
+    console.log(email.current.value);
+    console.log(password.current.value);
+    const message = checkValidData(
+      email.current.value,
+      password.current.value,
+      name.current.value
+    );
+    setErrorMessage(message);
   };
 
   return (
@@ -18,28 +36,38 @@ const Login = () => {
     >
       <Header />
       <div className="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] bg-black p-12 rounded-md bg-opacity-80">
-        <form className="flex flex-col gap-8 ">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="flex flex-col gap-8 "
+        >
           <h1 className="font-bold text-3xl py-4 text-white">
             {isSignInForm ? "Sign In " : "Sign Up"}
           </h1>
           {!isSignInForm && (
             <input
+              ref={name}
               type="text"
               placeholder="Full Name"
-              className="w-72 h-12 p-2 rounded-md bg-gray-700"
+              className="w-72 h-12 p-2 rounded-md bg-gray-700 text-white"
             />
           )}
           <input
             type="text"
             placeholder="Email Address"
-            className="w-72 h-12 p-2 rounded-md bg-gray-700"
+            ref={email}
+            className="w-72 h-12 p-2 rounded-md bg-gray-700 text-white"
           />
           <input
+            ref={password}
             type="password"
             placeholder="Password"
-            className="w-72 h-12 p-2 rounded-md bg-gray-700"
+            className="w-72 h-12 p-2 rounded-md bg-gray-700 text-white"
           />
-          <button className="p-3 bg-red-600 rounded-md">
+          <p className="text-red-500 font-bold text-lg py-1"> {errorMessage}</p>
+          <button
+            className="p-3 bg-red-600 rounded-md"
+            onClick={handleButtonClick}
+          >
             {isSignInForm ? "Sign In " : "Sign Up"}
           </button>
           <p
